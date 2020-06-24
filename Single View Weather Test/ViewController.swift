a4ac572f9bdee696e25e878d6da68b7def96b4dd
StasBelousov <stasbelousov@icloud.com>
2020-06-24 07:33:58 +0300

    Initial Commit

---
+++ Single View Weather Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Single View Weather Test
+//
+//  Created by Станислав Белоусов on 24/06/2020.
+//  Copyright © 2020 Станислав Белоусов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
